44a69e056ede7a48518a2ca4e5e5be335a86fb53
Drazen27 <103655456+Drazen27@users.noreply.github.com>
2024-05-08 14:15:20 -0400

    Mejora a la pagina principal

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -55,18 +55,18 @@ const NavBar: React.FC = () => {
           </button>
           </div>
           {isMenuOpen?
-            <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
+            <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-dark-purple ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
               <div className="py-1" role="none">
-                <a href="#"className="text-sky-blue hover:text-pink-panter block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id="menu-item-0">Inicio</a>
+                <a href="#"className="text-dark-purple hover:text-pink-panter block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id="menu-item-0">Inicio</a>
               </div>
               <div className="py-1" role="none">
-                <a href="#" className="text-sky-blue hover:text-pink-panter block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id="menu-item-1">¿Quiénes somos?</a>
+                <a href="#" className="text-dark-purple hover:text-pink-panter block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id="menu-item-1">¿Quiénes somos?</a>
               </div>
               <div className="py-1" role="none">
-                <a href="#" className="text-sky-blue hover:text-pink-panter block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id="menu-item-2">¿Qué ofrecemos?</a>
+                <a href="#" className="text-dark-purple hover:text-pink-panter block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id="menu-item-2">¿Qué ofrecemos?</a>
               </div>
               <div className="py-1" role="none">
-                <a href="#" className="text-sky-blue hover:text-pink-panter block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id="menu-item-3">Contactanos</a>
+                <a href="#" className="text-dark-purple hover:text-pink-panter block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id="menu-item-3">Contactanos</a>
               </div>
             </div>
            :
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,37 +9,44 @@ export default function Home() {
           <source src="https://video.wixstatic.com/video/0b340f_b4aaabafff194cf6ac9ee5511f58099d/720p/mp4/file.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="absolute top-0 left-0 w-full h-[600px] flex items-center justify-center">
-          <h1 className="text-white text-7xl font-bold">NEUROFASCINANTE</h1>
+        <div className="absolute top-0 left-0 w-full h-[600px] flex flex-wrap items-center justify-center">
+          <h1 className="text-white text-7xl font-sans font-bold">NEUROFASCINANTE</h1>
+          <h1 className="text-white text-7xl font-sans font-bold">NEUROFASCINANTE</h1>
         </div>
       </section>
       
-      <section className="container mx-auto mt-8 grid grid-cols-2 gap-4">
-        <div className="relative">
+      <section className="container mx-auto mt-8 grid grid-cols-5 gap-4">
+        <div className="relative col-span-3 ">
           <Image src={icons.LogoV3Trans} alt="Image 1" className="w-full h-auto" />
-          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-80">
+          <div className="absolute top-0 left-1/2 w-2/4 h-3/4 flex items-center justify-center bg-white">
             <h2 className="text-center text-xl font-bold">Imagen 1</h2>
           </div>
         </div>
-        <div className="relative">
+        <div className="relative col-span-2">
           <Image src={icons.LogoV3Trans} alt="Image 2" className="w-full h-auto" />
-          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-80">
+          <div className="absolute top-1/4 left-1/2 w-3/5 h-2/4 flex items-center justify-center bg-white">
             <h2 className="text-center text-xl font-bold">Imagen 2</h2>
           </div>
         </div>
       </section>
 
-      <section className="container mx-auto mt-8 grid grid-cols-2 gap-4">
-        <div className="relative">
+      <section className="container mx-auto mt-8 grid grid-cols-6 gap-4">
+        <div className="relative col-span-2">
           <Image src={icons.LogoV3Trans} alt="Image 3" className="w-full h-auto" />
-          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-80">
+          <div className="absolute top-0 left-1/4  w-3/5 h-3/4 flex items-center justify-center bg-white">
             <h2 className="text-center text-xl font-bold">Imagen 3</h2>
           </div>
         </div>
-        <div className="relative">
+        <div className="relative col-span-1">
           <Image src={icons.LogoV3Trans} alt="Image 4" className="w-full h-auto" />
-          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-80">
-            <h2 className="text-center text-xl font-bold">Imagen 4</h2>
+          <div className="absolute top-0 left-52  w-3/5 h-3/4 flex items-center justify-center bg-white">
+            <h2 className="text-center text-xl font-sans font-bold">Imagen 4</h2>
+          </div>
+        </div>
+        <div className="relative col-span-3">
+          <Image src={icons.LogoV3Trans} alt="Image 4" className="w-full h-auto" />
+          <div className="absolute top-0 left-52  w-3/5 h-3/4 flex items-center justify-center bg-white">
+            <h2 className="text-center text-xl font-sans font-bold">Imagen 4</h2>
           </div>
         </div>
       </section>

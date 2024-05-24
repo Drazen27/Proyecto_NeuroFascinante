bd24bdf3f1c1b2261a2ca2cf11c8f8436c86640e
Drazen27 <103655456+Drazen27@users.noreply.github.com>
2024-05-24 14:27:20 -0400

    Commit

--- src/app/componentes/informationCard.tsx
+++ src/app/componentes/informationCard.tsx
@@ -45,7 +45,7 @@ const InformationCards: FC<InformationCardsProps> = ({
             data-dropdown-toggle="dropdownInformation" 
             ref={ref}
             onClick={toggleMenu}
-            className={` bg-white flex flex-wrap h-fit    rounded-lg shadow w-[95%]
+            className={` bg-white flex flex-wrap h-fit    rounded-lg  w-[95%]
             ${isVisible ? animationIn : animationOut}`}
         >
             <div className='flex flex-col items-center w-full h-[95%] nv:flex-row'>
@@ -58,6 +58,7 @@ const InformationCards: FC<InformationCardsProps> = ({
                 <div className="flex flex-col px-10 pt-0 pb-4 w-full nv:w-[75%] ">
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h5 className="mb-2 text-center  text-2xl font-bold tracking-tight text-gray-900">{title}</h5>
+                        <hr/>
                         <p className="mb-3 font-normal text-center nv:text-justify text-gray-700">{desc}</p>
                         <p className="mb-3 font-normal text-center nv:text-justify text-gray-700">{desc2}</p>
                     </div>
@@ -74,9 +75,9 @@ const InformationCards: FC<InformationCardsProps> = ({
                 </div>
             
             </div>
-            {/* {
-            !isMenuOpen? */}
-            {/* <button className="w-full flex justify-center  font-medium px-5 py-2.5 mt-0" > 
+             {/* {
+            !isMenuOpen?
+             <button className="w-full flex justify-center  font-medium px-5 py-2.5 mt-0" > 
                     <svg className="w-5 h-5 ml-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="grey" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                     </svg>
@@ -86,22 +87,22 @@ const InformationCards: FC<InformationCardsProps> = ({
                 <svg className="w-5 h-5 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="grey" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5L5 1L9 5" />
                 </svg>
-            </button> */}
-            {/* } */}
-                {/* <div 
+            </button> 
+             } 
+                 <div 
                     id="dropdownInformation" 
                     className={`z-10 ${isMenuOpen ? 'block' : 'hidden'}  divide-y divide-gray-100 rounded-lg  w-full `}
                 >
                     <div className="py-3 px-4 nv:px-60 font-normal text-gray-900 text-center">
                         <div>{desc2}</div>
-                    </div> */}
-                    {/* <ul className="py-2 text-sm text-gray-700 " aria-labelledby="dropdownInformationButton">
+                    </div> 
+                     <ul className="py-2 text-sm text-gray-700 " aria-labelledby="dropdownInformationButton">
                         <li>
                             <a href="#" className="block px-4 py-2 hover:bg-gray-100 text-center ">Dashboard</a>
                         </li>
-                    </ul> */}
+                    </ul> 
 
-                {/* </div> */}
+                 </div>  */}
         </div>
     );
 };
---
+++ src/app/pages/information/terapiaAutismo/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import MisionVisionCards from "@/app/componentes/misionvisionCards";
+import AboutCards from "@/app/componentes/aboutCard";
+import InformationCards from "@/app/componentes/informationCard";
+export default function TerapiaAutismo() {
+  return (
+    <main className="mt-40">
+      <section className="flex flex-col justify-center items-center gap-6 mt-4">
+      <InformationCards 
+      img="'https://firebasestorage.googleapis.com/v0/b/neurofascinante-e0387.appspot.com/o/images%2Fneurofascinante%2FAutismo%202.png?alt=media&token=2c529e62-0b43-444d-b45c-e0233b8a1af8'"
+      title="TERAPIA NIÑOS CON AUTISMO"
+      desc="El Trastorno del Espectro Autista, denominado también autismo típico o autismo nuclear, está incluido dentro de los trastornos del neurodesarrollo. Éstos se manifiestan, normalmente, de manera precoz en el desarrollo, a menudo antes de que el niño/a empiece la escuela primaria, y se caracterizan por un déficit del desarrollo que produce deficiencias del funcionamiento personal, social, académico u ocupacional."
+      desc2="La intervención conductual intensiva temprana, la terapia cognitivo-conductual y el entrenamiento en destrezas sociales son tipos de programas conductuales que beneficiarán a los niños en su desarrollo general."
+      lista={[""]}
+      animationIn="animate-fadeLeft"
+      animationOut="animate-fadeOutLeft"
+      tresh={0.05}/>
+
+      </section>
+      
+    </main>
+  );
+}
